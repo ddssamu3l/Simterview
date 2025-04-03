e416487b58a03c8f9cabdc1fc5056f37ae39c747
邓敬熹 <samueldeng78@gmail.com>
2025-04-02 21:25:08 -0700

    changing front page tagline

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
       <section className="card-cta">
         <div className="flex flex-col gap-6 w-full items-center text-center">
           <h1 className="sm:text-5xl">Become FAANG-Ready With AI Mock Software Engineering Interviews</h1>
-          <p className="sm:text-lg">Test your technical & behavioral skills against live AI recruiters & get instant quality feedback</p>
+          <p className="sm:text-lg">Test your technical & behavioral skills against live AI recruiters & get instant quality feedback!</p>
           <Button asChild className="btn-primary w-full max-w-lg">
             <Link href="/interview-list">Start your interview now</Link>
           </Button>
